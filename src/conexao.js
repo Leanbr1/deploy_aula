fddddcd1dafead2324988ed6e8e5cbc1e4b63530
Leandro <brum1750@gmail.com>
2023-09-18 23:04:03 -0300

    adicionado config banco de dados

---
+++ src/conexao.js
@@ -0,0 +1,11 @@
+const knex = requrie('knex')({
+    client: 'pg',
+    connection: {
+        host: 'silly.db.elephantsql.com',
+        port: 5432,
+        user: 'jqoaqkqr',
+        password: 'postgres://jqoaqkqr:uAnUOY1uf7o5HjHgmtceq-v6z8gAhfXw@silly.db.elephantsql.com/jqoaqkqr',
+        database: 'jqoaqkqr'
+
+    }
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const knex = require('./conexao');
+
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json('ok');
+app.get('/', async (req, res) => {
+    try {
+        const carros = await knex('carros');
+        return res.json(carros);
+    } catch (error) {
+        return res.status(400).json({ mensagem: 'erro servidor' });
+    }
 })
 
 const port = process.env.PORT || 3000;
