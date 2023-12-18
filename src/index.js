96cff2b19b0289b8585051559c9095c5c3e16cbe
Leandro <brum1750@gmail.com>
2023-12-18 12:22:22 -0300

    teste commit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.get('/', async (req, res) => {
         return res.status(500).json({ mensagem: 'erro servidor' });
     }
 })
-
+console.log("oi teste 2");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`servidor em pe na porta ${port}`));
